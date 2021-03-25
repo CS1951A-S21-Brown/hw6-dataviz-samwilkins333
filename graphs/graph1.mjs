04b5c3deddd4ca88a6392ef7ba8ba5de9e04abf5
Sam Wilkins <samwilkins333@gmail.com>
2021-03-24 17:21:07 -0700

    switched to pie chart

--- graphs/graph1.mjs
+++ graphs/graph1.mjs
@@ -1,18 +1,19 @@
 let cleaned_data = undefined
 
 const width = MAX_WIDTH / 2,
-      height = MAX_HEIGHT / 2 - BUTTON_HEIGHT,
+      height = MAX_HEIGHT / 2 - BUTTON_HEIGHT - 10,
       duration = 500
 
-const label_offset = 2,
-      padding = 10;
+const padding = 10,
+      mar = 40;
 
 const svg = d3.select("#graph1")
     .append("svg")
-    .attr("width", width)
+    .attr("width", height)
     .attr("height", height)
+    .style("border-left", "1px solid #00000011")
     .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${height / 2}, ${height / 2})`);
 
 const x = d3.scaleLinear()
     .range([0, width - margin.left - margin.right]);
@@ -22,68 +23,114 @@ const y = d3.scaleBand()
     .padding(0.1);
 
 const countRef = svg.append("g");
-const y_axis_label = svg.append("g");
 
-svg.append("text")
-    .attr("transform", `translate(${(width - margin.left - margin.right) / 2}, ${height - margin.top - margin.bottom + 2 * padding})`)
-    .attr("font-size", "12px")
-    .attr("text-align", "center")
-    .style("text-anchor", "middle")
-    .text("Number of Titles");
-
-const y_axis_text = svg.append("text")
-    .attr("transform", `translate(${-5 * margin.left / 6}, ${(height - margin.top - margin.bottom) / 2}), rotate(-90)`)
-    .attr("font-size", "12px")
-    .style("text-anchor", "middle");
-
-const title = svg.append("text")
-    .attr("transform", `translate(0, -14)`)
+const title = d3.select("#title1").append("svg").attr("width", "350").attr("height", "34").append("text")
+    .attr("transform", `translate(140, 22)`)
     .style("font-weight", "bold")
-    .style("font-size", 15);
+    .style("font-size", 15)
+    .style("text-anchor", "middle")
+
+function focus(id, color) {
+    id = id.replace(/\s+/g, "")
+
+    for (const slice of [...document.getElementsByClassName("slice")]) {
+        slice.style.opacity = "0.2"
+    }
+    for (const item of [...document.getElementsByClassName("item")]) {
+        item.style.opacity = "0.2"
+    }
+    for (const target of [...document.getElementsByClassName(id)]) {
+        target.style.backgroundColor = color || "white"
+        target.style.opacity = "1"
+        target.scrollIntoView({ behavior: "smooth", block: "end" })
+        for (const child of [...target.children]) {
+            child.style.fontSize = "14px"
+        }
+    }
+}
+
+function relax() {
+    for (const slice of [...document.getElementsByClassName("slice")]) {
+        slice.style.opacity = "1"
+    }
+    for (const item of [...document.getElementsByClassName("item")]) {
+        item.style.opacity = "1"
+        item.style.backgroundColor = "white"
+        for (const child of [...item.children]) {
+            child.style.fontSize = "10px"
+        }
+    }
+}
 
 render_graph1 = async (category) => {
     const data = (cleaned_data = cleaned_data ?? clean_data(await d3.csv("../data/netflix.csv")))[category]
 
-    x.domain([0, d3.max(data, ({ count }) => count)]);
-    y.domain(data.map(({ genre }) => genre));
+    const pie_data_view = document.getElementById("pie-data")
+    pie_data_view.style.height = `${height}px`
 
-    y_axis_label.call(d3.axisLeft(y).tickSize(0).tickPadding(10));
-
-    const bars = svg.selectAll("rect").data(data);
+    const labels_list = document.getElementById("labels-list")
+    labels_list.innerHTML = ""
 
     const color = d3.scaleOrdinal()
         .domain(data.map(({ genre }) => genre))
-        .range(d3.quantize(d3.interpolateHcl("#ffcc33", "#66a0e2"), data.length));
+        .range(d3.quantize(d3.interpolateHcl("#ffcc33", "lightsteelblue"), data.length));
 
-    bars.enter()
-        .append("rect")
-        .merge(bars)
-        .transition()
-        .duration(duration)
-        .attr("fill", ({ genre }) => color(genre))
-        .attr("x", x(0))
-        .attr("y", ({ genre }) => y(genre))
-        .attr("width", ({ count }) => x(count))
-        .attr("height", y.bandwidth());
+    data.forEach((d, i) => {
+        const div = document.createElement("div")
+        const genre = document.createElement("span")
+        const count = document.createElement("span")
 
-    const counts = countRef.selectAll("text").data(data);
+        genre.style.fontSize = count.style.fontSize = "10px"
+        genre.style.padding = "0 2px 0 2px"
+        genre.style.borderRadius = "2px"
+        genre.textContent = `${i + 1}. ${d.genre}`
+        count.textContent = d.count
 
-    counts.enter()
-        .append("text")
-        .merge(counts)
-        .transition()
-        .duration(duration)
-        .attr("font-size", "10px")
-        .attr("x", ({ count }) => x(count) + label_offset)
-        .attr("y", ({ genre }) => y(genre) + 8)
-        .style("text-anchor", "start")
-        .text(({ count }) => count);
+        div.append(genre)
+        div.append(count)
 
-    y_axis_text.text(`${category} Genres`)
-    title.text(`Number of ${category}s Per Genre`);
+        const id = d.genre.replace(/\s+/g, "")
 
-    bars.exit().remove();
-    counts.exit().remove();
+        div.classList.add("item")
+        div.classList.add(id)
+
+        div.addEventListener("mouseenter", () => focus(d.genre, color(d.genre)))
+        div.addEventListener("mouseleave", relax)
+
+        labels_list.append(div)
+    })
+
+    const radius = (Math.min(width, height) + mar) / 2
+
+    const data_ready = d3.pie()
+        .sort(({ count }) => count)
+        .value(({ count }) => count)(data)
+
+    // noinspection JSCheckFunctionSignatures
+    const arc = d3.arc()
+      .innerRadius(radius * 0.5)
+      .outerRadius(radius * 0.8)
+
+    const slices = svg.selectAll('path').data(data_ready)
+
+    slices
+        .enter()
+        .append('path')
+        .merge(slices)
+        .attr('d', arc)
+        .attr('fill', ({ data }) => color(data.genre))
+        .attr("stroke", "white")
+        .attr("class", ({ data }) => `slice ${data.genre.replace(/\s+/g, "")}`)
+        .style("cursor", "pointer")
+        .style("stroke-width", "2px")
+        .on("mouseover", ({ data: { genre } }) => focus(genre, color(genre)))
+        .on("mouseout", relax)
+
+    title.text(`Number of Titles Per Genre (${category}s)`);
+
+    slices.transition().duration(duration)
+
+    slices.exit().remove()
 }
 
 function clean_data(data) {
